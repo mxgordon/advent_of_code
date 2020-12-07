6d1a2728083b5ea8a1bebd2e91ccf569e8bc4400
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-12-07 01:22:05 -0500

    Bigly better day 5

--- 2020/day5/part1.py
+++ 2020/day5/part1.py
@@ -1,4 +1,3 @@
-import math
 import json
 
 
@@ -8,27 +7,8 @@ with open("data.json", 'r') as f:
 max_id = 0
 
 for srow, scol in data:
-    row_min = 0
-    row_max = 127
-    for line in srow:
-        move_to = row_min + (row_max-row_min)/2
-        if line == "F":
-            row_max = math.floor(move_to)
-        else:
-            row_min = math.ceil(move_to)
-    row = row_min
-
-    col_min = 0
-    col_max = 7
-
-    for line in scol:
-        move_to = col_min + (col_max-col_min)/2
-        if line == "L":
-            col_max = math.floor(move_to)
-        else:
-            col_min = math.ceil(move_to)
-
-    col = col_min
+    row = int(srow.replace("F", "0").replace("B", "1"), 2)
+    col = int(scol.replace("L", "0").replace("R", "1"), 2)
 
     max_id = max(max_id, row*8 + col)
 
