6a4bdd8c7badb94116fdc9f880b6b5ac954e78fb
Max Gordon <tonald.drump2.0@gamil.com>
2021-12-10 10:18:19 -0500

    Day 10

--- 2021/day10/part1.py
+++ 2021/day10/part1.py
@@ -5,4 +5,23 @@ from collections import defaultdict, Counter
 
 with open("data.txt", 'r') as f:
     data = f.readlines()
+    data = list(map(list, map(str.strip, data)))
+
+brackets = {"{": "}", "[": "]", "(": ")", "<": ">"}
+points = 0
+pt_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
+
+for line in data:
+    opens = line[:1]
+
+    for e in line[1:]:
+        if e in brackets.keys():
+            opens.append(e)
+        else:
+            if e == brackets[opens[-1]]:
+                del opens[-1]
+            else:
+                points += pt_table[e]
+                break
+print(points)
 
--- 2021/day10/part2.py
+++ 2021/day10/part2.py
@@ -5,3 +5,27 @@ from collections import defaultdict, Counter
 
 with open("data.txt", 'r') as f:
     data = f.readlines()
+    data = list(map(list, map(str.strip, data)))
+
+brackets = {"{": "}", "[": "]", "(": ")", "<": ">"}
+points = []
+pt_table = {")": 1, "]": 2, "}": 3, ">": 4}
+
+for line in data:
+    opens = line[:1]
+
+    for e in line[1:]:
+        if e in brackets.keys():
+            opens.append(e)
+        else:
+            if e == brackets[opens[-1]]:
+                del opens[-1]
+            else:
+                break
+    else:
+        if len(opens) != 0:
+            closers = list(map(pt_table.__getitem__, map(brackets.__getitem__, opens[::-1])))
+            points.append(reduce(lambda i, n: (i*5)+n, closers, 0))
+points.sort()
+print(points[len(points)//2])
+
